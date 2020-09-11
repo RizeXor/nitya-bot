b710ebb6c11f8d6c4e9ca738cb680ac8d58b9107
RizeXor <decodestudios.help@gmail.com>
2020-09-11 17:29:13 +0530

    bu

--- main.py
+++ main.py
@@ -13,27 +13,40 @@ async def on_ready():
     print('bot is ready')
 
 
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Please enter a number')
+    elif isinstance(error, commands.CommandNotFound):
+        await ctx.send('ur mom')
+    else:
+        print(error)
+        await ctx.send('Error')
+
+
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
 
+@bot.command(name='urmom')
+async def ur_mom(ctx):
+    await ctx.send('is fat')
+
+
 @bot.command()
 async def help(ctx):
-    await ctx.send('ur mom')
+    emoji = bot.get_emoji(750548895146180659)
+    await ctx.send(emoji)
 
 
 @bot.command(name='pottyX')
-async def pottyx(ctx, spamAmount):
-    if not spamAmount.isnumeric():
-        await ctx.send('Please enter a number')
-        return
-    spamAmount = int(spamAmount)
-    if spamAmount <= 0 or spamAmount > 25:
-        await ctx.send('Please enter a number between 1 and 25')
-        return
-    for _ in range(spamAmount):
-        await ctx.send('<@362862038042673152>')
+async def pottyx(ctx, spamAmount: int):
+    print(f"called by {ctx.author}")
+    emoji = bot.get_emoji(729563457892122787)
+    for x in range(spamAmount):
+        await ctx.send(f'sexy boi arya {emoji} <@362862038042673152> ({x + 1})'
+                       )
 
 
 bot.run(os.getenv("TOKEN"))
